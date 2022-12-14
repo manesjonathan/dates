4c86b626b93d5263fe345b8297abe409a82fb3c7
Jonathan Manes <manesjona@gmail.com>
2022-12-14 09:04:35 +0100

    fin

--- Bonus/assets/js/script.js
+++ Bonus/assets/js/script.js
@@ -4,13 +4,11 @@ let monthStamp = document.querySelector(".month");
 let dayStamp = document.querySelector(".day");
 let dayOfWeekStamp = document.querySelector(".day-of-week");
 let is24 = true;
-let params = { hour12: false, hour: "2-digit", minute: "2-digit" };
-
+let params = { hour12: false, hour: "numeric", minute: "numeric", second: "numeric" };
 
 function displayTime() {
     let date = new Date();
     timeStamp.innerHTML = date.toLocaleTimeString("en-US", params);
-    timeStamp.innerHTML += ":" + date.getSeconds();
     yearStamp.innerHTML = date.getFullYear();
     monthStamp.innerHTML = (date.toLocaleDateString("en-US", { month: "short" }));
     dayStamp.innerHTML = date.getDate()
@@ -18,15 +16,33 @@ function displayTime() {
 }
 
 timeStamp.addEventListener("click", () => {
+    let date = new Date();
+
     if (is24) {
-        params = { hour12: false, hour: "2-digit", minute: "2-digit" };
+        params = { hour12: false, hour: "numeric", minute: "2-digit", second: "numeric" };
+        timeStamp.innerHTML = date.toLocaleTimeString("en-US", params);
         is24 = false
 
     } else {
-        params = { hour12: true, hour: "2-digit", minute: "2-digit" };
+        params = { hour12: true, hour: "numeric", minute: "2-digit", second: "numeric" };
+        //todo
+        let newTimeStamp = date.toLocaleTimeString("en-US", params).trim();
+        console.log(newTimeStamp);
+        timeStamp.innerHTML = date.toLocaleTimeString("en-US", params);
         is24 = true
 
-        console.log(params);
     }
 });
+
 setInterval(displayTime, 1000);
+
+function countdown() {
+    let date = new Date();
+    let futureDate = new Date("2022/12/14 06:00:00");
+    let countdown = Number(futureDate - date);
+    timeStamp.innerHTML = new Date(countdown).toLocaleTimeString("en-US", params);
+    yearStamp.innerHTML = date.getFullYear();
+    monthStamp.innerHTML = (date.toLocaleDateString("en-US", { month: "short" }));
+    dayStamp.innerHTML = futureDate.getDate();
+    dayOfWeekStamp.innerHTML = date.toLocaleDateString("en-US", { weekday: "short" });
+}
